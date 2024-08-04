d4261f3572e7970e5615eb17003b5a7e83236f84
mcombeau <mcombeau@student.42.fr>
2024-08-04 16:31:01 +0200

    refactor: generate list of root servers + use arrays instead of slices

--- internal/dnsResolver/root_servers.go
+++ internal/dnsResolver/root_servers.go
@@ -1,51 +1,51 @@
 package dnsResolver
 
 import (
+	"fmt"
 	"log"
 	"net"
+	"net/netip"
 	"time"
 
 	"github.com/mcombeau/dns-tools/dns"
 )
 
 const publicDNSServer = "1.1.1.1:53"
+const numberOfRootServers = 13
 
-var rootServers []string
+var rootServers [numberOfRootServers]string
 
 func FetchRootServers() {
 	// TODO: Maybe run this periodically to update root servers?
-	rootServerNames := []string{
-		"a.root-servers.net.",
-		"b.root-servers.net.",
-		"c.root-servers.net.",
-		"d.root-servers.net.",
-		"e.root-servers.net.",
-		"f.root-servers.net.",
-		"g.root-servers.net.",
-		"h.root-servers.net.",
-		"i.root-servers.net.",
-		"j.root-servers.net.",
-		"k.root-servers.net.",
-		"l.root-servers.net.",
-		"m.root-servers.net.",
-	}
+	rootServerNames := getRootServerNames()
 
-	var rootServerIPs []string
-	for _, serverName := range rootServerNames {
+	var rootServerIPs [numberOfRootServers]string
+	var count int
+
+	for i, serverName := range rootServerNames {
 		ip, err := resolveWithPublicDNS(serverName)
 		if err != nil {
 			log.Printf("failed to resolve IP for server %s: %v", serverName, err)
 			continue
 		}
-		rootServerIPs = append(rootServerIPs, ip)
+
+		rootServerIPs[i] = ip
+		count++
 	}
 
-	if len(rootServerIPs) < 1 {
+	if count < 1 {
 		log.Panic("could not resolve IPs for root servers")
 	}
 
 	rootServers = rootServerIPs
 	log.Printf("fetched root servers: %v", rootServers)
+}
+
+func getRootServerNames() (rootServerNames [numberOfRootServers]string) {
+	for i := 0; i < numberOfRootServers; i++ {
+		rootServerNames[i] = fmt.Sprintf("%c.root-servers.net.", 'a'+i)
+	}
+	return rootServerNames
 }
 
 func resolveWithPublicDNS(serverName string) (ip string, err error) {
@@ -65,17 +65,23 @@ func resolveWithPublicDNS(serverName string) (ip string, err error) {
 		return "", err
 	}
 
-	response := make([]byte, 512)
+	response := [dns.MaxDNSMessageSizeOverUDP]byte{}
 	conn.SetReadDeadline(time.Now().Add(5 * time.Second))
-	_, err = conn.Read(response)
+	n, err := conn.Read(response[:])
 	if err != nil {
 		return "", err
 	}
 
-	decodedResponse, err := dns.DecodeMessage(response)
+	decodedResponse, err := dns.DecodeMessage(response[:n])
 	if err != nil {
 		return "", err
 	}
 
-	return decodedResponse.Answers[0].RData.String(), nil
+	ip = decodedResponse.Answers[0].RData.String()
+	_, err = netip.ParseAddr(ip)
+	if err != nil {
+		return "", err
+	}
+
+	return ip, nil
 }
