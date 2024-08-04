92ca687928e4f4cb418401dacc430b8d3726c7d3
mcombeau <mcombeau@student.42.fr>
2024-08-04 17:20:51 +0200

    feat: forward client DNS request to root server

--- internal/dnsResolver/resolver.go
+++ internal/dnsResolver/resolver.go
@@ -1,1 +1,46 @@
 package dnsResolver
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"time"
+)
+
+func ResolveDNSQuery(dnsRequest []byte) (response []byte, err error) {
+	log.Printf("ResolveDNSQuery() got DNS query: %v", dnsRequest)
+
+	for _, rootServer := range rootServers {
+		rootAddr, err := net.ResolveUDPAddr("udp", rootServer+":53")
+		if err != nil {
+			log.Printf("failed to resolve root server address: %v", err)
+			continue
+		}
+
+		conn, err := net.DialUDP("udp", nil, rootAddr)
+		if err != nil {
+			log.Printf("failed to dial root server: %v", err)
+			continue
+		}
+		defer conn.Close()
+
+		_, err = conn.Write(dnsRequest)
+		if err != nil {
+			log.Printf("failed to send DNS request to root server: %v", err)
+			continue
+		}
+
+		receivedResponse := [4096]byte{}
+		conn.SetReadDeadline(time.Now().Add(5 * time.Second))
+		n, err := conn.Read(receivedResponse[:])
+		if err != nil {
+			log.Printf("failed to read response from root server: %v", err)
+			continue
+		}
+		fmt.Printf("Response received from root server was length: %d\n", n)
+
+		return receivedResponse[:n], nil
+	}
+
+	return response, fmt.Errorf("failed to get responses from any root server")
+}
--- internal/dnsServer/server.go
+++ internal/dnsServer/server.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"log"
 	"net"
+
+	"github.com/mcombeau/dns-resolver/internal/dnsResolver"
 )
 
 const DNSResolverIP = "0.0.0.0"
@@ -27,12 +29,27 @@ func StartUDPServer() (err error) {
 
 	buffer := [MaxUDPMessageLength]byte{}
 	for {
-		_, clientAddr, err := conn.ReadFromUDP(buffer[:])
+		n, clientAddr, err := conn.ReadFromUDP(buffer[:])
 		if err != nil {
 			log.Printf("error reading from UDP: %v", err)
 			continue
 		}
-		// TODO: handle client request here
-		log.Printf("read from client %s:%d over UDP: %v", clientAddr.IP.String(), clientAddr.Port, buffer)
+
+		// TODO: Make this concurrent to handle multiple simultanous requests
+		handleDNSRequest(conn, clientAddr, buffer[:n])
+	}
+}
+
+func handleDNSRequest(conn *net.UDPConn, clientAddr *net.UDPAddr, request []byte) {
+	response, err := dnsResolver.ResolveDNSQuery(request)
+	if err != nil {
+		log.Printf("failed to resolve DNS request from client %v: %v", clientAddr, err)
+		return
+	}
+
+	_, err = conn.WriteToUDP(response, clientAddr)
+	if err != nil {
+		log.Printf("failed to send response to client %v: %v", clientAddr, err)
+		return
 	}
 }
