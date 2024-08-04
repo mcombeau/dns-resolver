64fc48ac772b1f15a223f2b2fb434875e456ffec
mcombeau <mcombeau@student.42.fr>
2024-08-04 13:06:49 +0200

    init: setup project

---
+++ cmd/main.go
@@ -0,0 +1,15 @@
+package main
+
+// TODO: Gameplan:
+// - Setup server as listener on port 5353
+// - Query public DNS like 8.8.8.8 or 1.1.1.1 for root servers
+// - When a query arrives:
+// 		- query root servers, parse response,
+// 		- query next server, parse response,
+// 		- etc until authoritative response.
+// Later:
+// 		- add caching
+//		- handle multiple concurrent client requests
+
+func main() {
+}
