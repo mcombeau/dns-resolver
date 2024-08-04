687f81f3daec346fe7dd8a098e261511a7aaaafe
mcombeau <mcombeau@student.42.fr>
2024-08-04 19:10:36 +0200

    WIP: add recursive server querying logic

--- internal/dnsResolver/resolver.go
+++ internal/dnsResolver/resolver.go
@@ -4,43 +4,94 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"net/netip"
 	"time"
+
+	"github.com/mcombeau/dns-tools/dns"
 )
 
 func ResolveDNSQuery(dnsRequest []byte) (response []byte, err error) {
 	log.Printf("ResolveDNSQuery() got DNS query: %v", dnsRequest)
 
-	for _, rootServer := range rootServers {
-		rootAddr, err := net.ResolveUDPAddr("udp", rootServer+":53")
-		if err != nil {
-			log.Printf("failed to resolve root server address: %v", err)
-			continue
-		}
-
-		conn, err := net.DialUDP("udp", nil, rootAddr)
-		if err != nil {
-			log.Printf("failed to dial root server: %v", err)
-			continue
-		}
-		defer conn.Close()
-
-		_, err = conn.Write(dnsRequest)
-		if err != nil {
-			log.Printf("failed to send DNS request to root server: %v", err)
-			continue
-		}
-
-		receivedResponse := [4096]byte{}
-		conn.SetReadDeadline(time.Now().Add(5 * time.Second))
-		n, err := conn.Read(receivedResponse[:])
-		if err != nil {
-			log.Printf("failed to read response from root server: %v", err)
-			continue
-		}
-		fmt.Printf("Response received from root server was length: %d\n", n)
-
-		return receivedResponse[:n], nil
+	response, err = queryServers(rootServers[:], dnsRequest)
+	if err != nil {
+		return nil, err
 	}
 
 	return response, fmt.Errorf("failed to get responses from any root server")
+}
+
+func queryServers(serverList []string, dnsRequest []byte) (response []byte, err error) {
+	for _, server := range serverList {
+		fmt.Printf("============ QUERYING SERVER %s\n", server)
+		response, err := sendDNSQuery(server, dnsRequest)
+		if err != nil {
+			log.Printf("failed to query server %s: %v", server, err)
+			continue
+		}
+
+		dnsParsedResponse, err := dns.DecodeMessage(response)
+		if err != nil {
+			log.Printf("failed to parse response from server %s: %v", server, err)
+			return nil, err
+		}
+
+		if len(dnsParsedResponse.Answers) > 0 {
+			fmt.Print("-------- GOT ANSWER\n")
+			dns.PrintMessage(dnsParsedResponse)
+			return response, nil
+		}
+
+		if len(dnsParsedResponse.NameServers) > 0 {
+			authorityServers := extractAuthorityServers(dnsParsedResponse)
+			if len(authorityServers) > 0 {
+				return queryServers(authorityServers, dnsRequest)
+			}
+		}
+	}
+	return nil, fmt.Errorf("failed to resolve DNS query")
+}
+
+func extractAuthorityServers(dnsMessage dns.Message) (serverList []string) {
+	for _, authority := range dnsMessage.NameServers {
+		if authority.RType == dns.NS {
+			nsRecord := authority.RData.String()
+			_, err := netip.ParseAddr(nsRecord)
+			if err == nil {
+				serverList = append(serverList, nsRecord)
+
+			}
+		}
+	}
+	fmt.Printf("--- SERVER LIST: %v\n", serverList)
+	return serverList
+}
+
+func sendDNSQuery(server string, dnsRequest []byte) (response []byte, err error) {
+	serverAddr, err := net.ResolveUDPAddr("udp", server+":53")
+	if err != nil {
+		return nil, fmt.Errorf("failed to resolve server address: %w", err)
+	}
+
+	conn, err := net.DialUDP("udp", nil, serverAddr)
+	if err != nil {
+		return nil, fmt.Errorf("failed to dial server: %w", err)
+	}
+	defer conn.Close()
+
+	_, err = conn.Write(dnsRequest)
+	if err != nil {
+		return nil, fmt.Errorf("failed to send DNS request to server: %w", err)
+	}
+
+	receivedResponse := [4096]byte{}
+	conn.SetReadDeadline(time.Now().Add(5 * time.Second))
+	n, err := conn.Read(receivedResponse[:])
+	if err != nil {
+		return nil, fmt.Errorf("failed to read response from root server: %w", err)
+	}
+
+	fmt.Printf("Response received from root server was length: %d\n", n)
+
+	return receivedResponse[:n], nil
 }
